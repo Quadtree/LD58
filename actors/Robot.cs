2fb6618f09eb75e1f2bca4b819b0c86d411d16d5
Quadtree <quadtree@gmail.com>
2025-10-04 11:07:33 -0700

    U

--- actors/Robot.cs
+++ actors/Robot.cs
@@ -42,6 +42,8 @@ public partial class Robot : CharacterBody3D
         else
             MotionMode = MotionModeEnum.Grounded;
 
+        GD.Print(GlobalPosition);
+
         // if (Mathf.Abs(robotControl.X) > 0.01f)
         // {
         //     AngularVelocity = new Vector3(
