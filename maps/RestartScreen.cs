b93a5fa4fa4cc686898e8a0c6f25c0f61f83d3d1
Quadtree <quadtree@gmail.com>
2025-10-04 11:55:09 -0700

    U

---
+++ maps/RestartScreen.cs
@@ -0,0 +1,24 @@
+using System;
+using Godot;
+
+public partial class RestartScreen : Control
+{
+    float Cooldown = 1;
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+
+        Cooldown -= (float)delta;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        if (@event is InputEventKey k)
+        {
+            if (Cooldown <= 0) GetTree().ChangeSceneToFile("res://maps/Default.tscn");
+        }
+    }
+}
