ee197477586a69b5a829b03b10c98abaa2d50cc9
Quadtree <quadtree@gmail.com>
2025-10-04 11:50:18 -0700

    U

---
+++ actors/ExitPortal.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using Godot;
+
+public partial class ExitPortal : Area3D
+{
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        var nearbyArtifacts = GetTree().CurrentScene.FindChildrenByPredicate<Artifact>(it => it.GlobalPosition.DistanceTo(GlobalPosition) < 5).Count();
+
+        var light = this.FindChildByType<OmniLight3D>();
+        light.LightEnergy = nearbyArtifacts * .33f;
+
+        if (GetOverlappingBodies().OfType<PlayerCharacter>().Any())
+        {
+            GD.Print("PC is in area");
+        }
+    }
+
+
+}
